216bb28e84ba6b07dc27918b982d78754da26a5b
Yeo Xin Ee Araki <84427221+Arakii0@users.noreply.github.com>
2024-08-22 15:11:41 +0800

    create template

---
+++ hash_analyzer.py
@@ -0,0 +1,39 @@
+import hashlib
+
+def identify(hash):
+    pass
+
+def hash_string(string):
+    pass
+
+def crack_hash(hash):
+    pass
+
+
+
+print("""
+ ___  ___  ________  ________  ___  ___  ________  ___       ___          
+|\  \ \  \|\   __  \|\   ____\|\  \ \  \|\   __  \|\  \     /\  \         
+\ \  \_\  \ \  \ \  \ \  \___|\ \  \_\  \ \  \ \  \ \  \    \ \  \        
+ \ \   __  \ \   __  \ \_____  \ \   __  \ \   __  \ \  \    \ \  \       
+  \ \  \ \  \ \  \ \  \|____|\  \ \  \ \  \ \  \ \  \ \  \____\ \  \____  
+   \ \__\ \__\ \__\ \__\____\_\  \ \__\ \__\ \__\ \__\ \_______\ \_______\,
+    \|__|\|__|\|__|\|__|\_________\|__|\|__|\|__|\|__|\|_______|\|_______|
+                       \|_________|                                       """)
+
+print("Please select an option:")
+user_input = input("""1. Identify Hash
+2. Hash     
+3. Crack Hash
+Select an option: """)
+
+match user_input:
+    case 1:
+        pass
+    case 2:
+        pass
+    case 3:
+        pass
+    case _:
+        print("Choose an option above!")
+
