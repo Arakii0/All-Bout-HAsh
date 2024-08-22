46833397300dc061144d1725955a308bae752243
Yeo Xin Ee Araki <84427221+Arakii0@users.noreply.github.com>
2024-08-22 22:09:18 +0800

    Completed cracking of hash

--- hash_analyzer.py
+++ hash_analyzer.py
@@ -26,8 +26,53 @@ def hash_string(string):
 
 
 
-def crack_hash(hash):
-    pass
+def crack_hash(hash, dictionary=None):
+    
+    hashes = sorted(list(hashlib.algorithms_guaranteed))
+    count = 1
+    for i in hashes:
+        print(f"{count}: {i}")
+        count += 1
+    hash_selection = int(input("Select the hash type: "))
+
+    print("Cracking hash...")
+
+    cracked = False
+
+    
+
+    if dictionary:
+        with open(dictionary, "r") as f:
+            for password in f:
+                password = password.strip()
+                encoded = password.encode()
+
+                h = hashlib.new(hashes[hash_selection - 1])
+                h.update(encoded)
+                
+                if hash == h.hexdigest():
+                    cracked = True
+                    print(f"Hash cracked: {password}")
+                    break
+
+
+    else:
+        dictionary = ['123456', 'password', '12345678', 'qwerty', '123456789', '12345', '1234', '111111', '1234567', 'dragon', '123123', 'baseball', 'abc123', 'football', 'monkey', 'letmein', '696969', 'shadow', 'master', '666666', 'qwertyuiop', '123321', 'mustang', '1234567890', 'michael', '654321', 'pussy', 'superman', '1qaz2wsx', '7777777', 'fuckyou', '121212', '000000', 'qazwsx', '123qwe', 'killer', 'trustno1', 'jordan', 'jennifer', 'zxcvbnm', 'asdfgh', 'hunter', 'buster', 'soccer', 'harley', 'batman', 'andrew', 'tigger', 'sunshine', 'iloveyou', 'fuckme', '2000', 'charlie', 'robert', 'thomas', 'hockey', 'ranger', 'daniel', 'starwars', 'klaster', '112233', 'george', 'asshole', 'computer', 'michelle', 'jessica', 'pepper', '1111', 'zxcvbn', '555555', '11111111', '131313', 'freedom', '777777', 'pass', 'fuck', 'maggie', '159753', 'aaaaaa', 'ginger', 'princess', 'joshua', 'cheese', 'amanda', 'summer', 'love', 'ashley', '6969', 'nicole', 'chelsea', 'biteme', 'matthew', 'access', 'yankees', '987654321', 'dallas', 'austin', 'thunder', 'taylor', 'matrix', 'minecraft']
+        for password in dictionary:
+            encoded = password.encode()
+
+            h = hashlib.new(hashes[hash_selection - 1])
+            h.update(encoded)
+
+            if hash == h.hexdigest():
+                cracked = True
+                print(f"Hash cracked: {password}")
+                break
+    
+    if not cracked:
+        print("Exhausted wordlist")
+
+                
 
 
 
@@ -51,10 +96,9 @@ match user_input:
     case 1:
         pass
     case 2:
-        user_input = input("Please enter a string to be hashed: ")
-        hash_string(user_input)
+        hash_string(input("Please enter a string to be hashed: "))
     case 3:
-        pass
+        crack_hash(input("Enter the hash: "))
     case _:
         print("Choose an option above!")
 
